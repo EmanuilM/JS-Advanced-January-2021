31f285325c74ba2a518c428338d2199c8affcb7e
Emanuil Mladenov <71393659+ExGiX@users.noreply.github.com>
2021-01-18 21:11:39 +0200

    Create 03.cityTaxes.js

---
+++ Labs/03.Objects and Composition/03.cityTaxes.js
@@ -0,0 +1,21 @@
+function solve(name , population , treasury) {
+    let output = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes: function() {
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth: function(percentage) {
+            this.population += (this.population * percentage) / 100;
+           
+        },
+        applyRecession: function(percentage) {
+            this.treasury -= (this.treasury * percentage) / 100;
+        }
+     }
+    
+     return output;
+
+}
