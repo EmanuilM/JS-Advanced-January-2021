a87c6d1166a318a52d69f7ad0346bae59d84f77f
Emanuil Mladenov <71393659+ExGiX@users.noreply.github.com>
2021-01-20 15:25:56 +0200

    Create 08.rectangle.js

---
+++ Exercises/03.Objects and Composition/08.rectangle.js
@@ -0,0 +1,10 @@
+function solve(width,height , color) {
+    return {
+        width : width,
+        height : height ,
+        color: color[0].toUpperCase() + color.slice(1),
+        calcArea() {
+            return this.width * this.height
+        }
+    }
+}
