7ac08c188654829f1f753a36570c8d63bfdcc7c0
Emanuil Mladenov <71393659+ExGiX@users.noreply.github.com>
2021-01-22 17:26:37 +0200

    Create ShowMore.js

---
+++ Labs/04.DOM Introduction/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+  let linkElement = document.getElementById('more');
+  let textElement = document.getElementById('text');
+  textElement.style.display = 'inline';
+  linkElement.style.display = 'none';
+  
+}
