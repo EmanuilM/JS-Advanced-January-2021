4364c3825c3b984618cdc463c1f160ef05df5bca
Emanuil Mladenov <71393659+ExGiX@users.noreply.github.com>
2021-01-23 17:31:14 +0200

    Create subtract.js

---
+++ Exercises/04.DOM Introduction/01. Subtraction/subtract.js
@@ -0,0 +1,5 @@
+function subtract() {
+    let numOne = document.getElementById('firstNumber').value
+    let numTwo = document.getElementById('secondNumber').value;
+    document.getElementById('result').innerHTML = numOne - numTwo;
+}
