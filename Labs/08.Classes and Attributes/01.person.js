37dbc87a48de043bd82870bd9925d798a2e4071f
Emanuil Mladenov <71393659+ExGiX@users.noreply.github.com>
2021-02-07 16:57:46 +0200

    Create 01.person.js

---
+++ Labs/08.Classes and Attributes/01.person.js
@@ -0,0 +1,9 @@
+class Person{ 
+     constructor(firstName,lastName,age,email) { 
+        Object.assign(this,{firstName,lastName,age,email});
+     }
+
+     toString() { 
+         return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+     }
+}
