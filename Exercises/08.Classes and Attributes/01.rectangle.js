40b291015b700bd1f8b9f620416dcab9328ed9e4
Emanuil Mladenov <71393659+ExGiX@users.noreply.github.com>
2021-02-09 20:27:21 +0200

    Update 01.rectangle.js

--- Exercises/08.Classes and Attributes/01.rectangle.js
+++ Exercises/08.Classes and Attributes/01.rectangle.js
@@ -1,1 +1,8 @@
-
+class Rectangle {
+    constructor(width, height, color) {
+        Object.assign(this, {width, height, color: color[0].toUpperCase() + color.slice(1)});
+    }
+    calcArea() {
+        return this.width * this.height;
+    }
+}
